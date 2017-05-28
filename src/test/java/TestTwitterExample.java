4f64a0d7c6acc526a8ca6861f9a15425ba5c9f08
isaacmg <igodfried@isaac26.com>
2017-05-28 19:57:16 -0400

    updataje ww

--- src/test/java/TestTwitterExample.java
+++ src/test/java/TestTwitterExample.java
@@ -17,5 +17,12 @@ public class TwitterExampleTest {
         assertEquals("theList must be a word",a.initArrayList("test.txt"),theList);
 
     }
+    public  void testEqualize(){
+        TwitterExample a = new TwitterExample();
+        String one = a.tokenize("the23123...");
+        assertEquals("string one should be test", a.tokenize(one), "test");
+        assertEquals("string two should be whatwe", a.tokenize("whatwe"), "whatwe")
+
+    }
 
 }
