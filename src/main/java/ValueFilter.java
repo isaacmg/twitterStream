dcd7fe56870ac3a2526c7f2cc75258a4e916eb78
isaacmg <igodfried@isaac26.com>
2017-11-29 01:38:22 -0500

    Fixed filt

--- src/main/java/ValueFilter.java
+++ src/main/java/ValueFilter.java
@@ -1,23 +1,26 @@
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+
+import sun.java2d.pipe.SpanShapeRenderer;
 
 /**
  * Created by isaac on 11/26/17.
  */
-abstract class ValueFilter extends SimpleCondition {
-    protected int value;
-    ValueFilter(int val) {
-        value = val;
+
+
+
+
+class SimpleCondition2<String> extends SimpleCondition<String> {
+
+    SimpleCondition2(int val){
+
     }
 
-}
-//Solve stupid object type problem even though nothing wrong
-class ValueTupleFilter extends ValueFilter{
-
-    public boolean filter(Tuple2<String,Integer> subEvent) {
-        int i = subEvent.getField(1);
-        return i > value;
+    public boolean filter(String value) throws Exception {
+        return false;
     }
-
 }
+
+
 
